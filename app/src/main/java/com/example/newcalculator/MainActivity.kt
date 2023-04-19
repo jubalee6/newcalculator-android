598d5cc2d47120a90404b59b21913db712d86f1e
jubalee6 <jubalandev@gmail.com>
2023-04-19 17:59:16 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/newcalculator/MainActivity.kt
+++ app/src/main/java/com/example/newcalculator/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.newcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edittext = EditText(this)
+        val button = Button(this)
+        button.setOnClickListener {
+            edittext.append("1")
+        }
+
+        /*   button.setOnClickListener {
+
+        }
+
+        val sum = calculate(1, 2)
+        sum
+
+
+
+
+        calculateAction(1, 2) { sum ->
+        }
+    }
+
+
+    private fun calculate(first:Int, second: Int): Int {
+        return first + second
+    }
+
+    private fun calculateAction(first: Int, second: Int, action: (Int) -> Unit) {
+        action(first + second)
+    }*/
     }
 }
